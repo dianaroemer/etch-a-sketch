2f95e3ca9803aeb5a835167ded3e8c3f05dd0603
D Roemer <dominicroemer@gmail.com>
2021-08-16 18:16:49 -0700

    Clean code, remove useless lines, add clarity comments

--- index.js
+++ index.js
@@ -1,55 +1,33 @@
 // Goal 1: Create 16 Divs inside of "container"
 
+// Create container pointer for reference elements
 const container = document.querySelector('.container');
 
-// console.log(container);
-
+// Create initial 4x4 grid using for loop, adding elements to div before appending to container.
 for (let i = 0; i < 16; i++ ) {
     const div = document.createElement(`div`);
     div.setAttribute('id', `block${i}`)
-    // div.textContent = i;
 
+    // Style newly created divs
     div.setAttribute( 'style',
-        `//color: blue;
-        background-color:black;
-        // border: solid;
-        // border-color: green;
-        margin: 1px 1px 1px 1px;
-        `
+        `background-color:black;`
+        // margin: 1px 1px 1px 1px;
     )
 
+    // Add mouseover event listener and timeout function, changing color of object on mouseover, and after Timeout, reverting to original color
     div.addEventListener('mouseover', (e) => {
-        // console.log(e.target);
-        // div.setAttribute('style', 
-        //     `background-color: white;
-        //     margin: 1px 1px 1px 1px;`
-        // );
-
         e.target.style.backgroundColor = "white";
-
         
+        // After brief timeout in ms, revert color to initial black
         setTimeout(function() {
             e.target.style.backgroundColor = "black";
         }, 500);
 
     });
 
-    // div.addEventListener('mouseout', (e) => {
-    //     console.log(e.target);
-    //     div.setAttribute('style', 
-    //         `background-color: black; 
-    //         margin: 1px 1px 1px 1px;`
-    //     );
-    // });
-
-
-
-
+    // Append newly created and styled element to parent container
     container.appendChild(div);
-
-
 }
 
 const containerList = document.querySelectorAll(('div'));
 
-console.log(containerList);
